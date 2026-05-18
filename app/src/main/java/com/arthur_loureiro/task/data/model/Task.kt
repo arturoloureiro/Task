d385d60783fcbd8c028472ed08d8abae8665eac5
arturoloureiro <arthurloureirolima@gmail.com>
2026-05-18 18:29:53 -0300

    Passo 180.

---
+++ app/src/main/java/com/arthur_loureiro/task/data/model/Task.kt
@@ -0,0 +1,6 @@
+package com.arthur_loureiro.task.data.model
+
+data class Task (
+    val id: String,
+    val description: String
+)
---
+++ app/src/main/java/com/arthur_loureiro/task/ui/adapter/TaskAdapter.kt
@@ -0,0 +1,24 @@
+package com.arthur_loureiro.task.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.arthur_loureiro.task.databinding.ItemTaskBinding
+
+class TaskAdapter : RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    inner class MyViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
+    }
+
+}
